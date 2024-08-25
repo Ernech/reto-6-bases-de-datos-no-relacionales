038a5fe5934c8659b6f5e49b0eab39e8280d9b0e
Ernesto Vilela <ernestovilela@MacBook-Pro-de-Ernesto.local>
2024-08-24 22:02:37 -0400

    server class created

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('Hello world');
+import { envs } from "./config/envs";
+import { MongoDatabase } from "./data/mongodb";
+import { Server } from "./presentation/server"
+
+
+()=>{}
+
+
+async function main(){
+
+    await MongoDatabase.connect({
+        dbName:envs.MONGO_DB_NAME,
+        mongoUrl:envs.MONGO_URL
+    })
+    //Todo: Server start 
+    new Server({port:envs.PORT}).start();
+}
---
+++ src/presentation/server.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+
+interface Options{
+    port?:number,
+
+}
+
+
+export class Server{
+
+    private readonly app = express()
+    private readonly port:number;
+
+    constructor(options:Options){
+        const{port=3000} = options;
+        this.port=port;
+    }
+
+    async start(){
+         //Middlewares
+         this.app.use(express.json());
+         this.app.use(express.urlencoded({extended:true}))
+         //Middlewares
+        
+         this.app.use(express.json());
+         this.app.use(express.urlencoded({extended:true}))
+ 
+    }
+}
