71987f13f1325ad2530d674f6a7267553711e18b
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-08-26 21:51:11 -0400

    Creating restaurant entity

---
+++ src/domain/entities/restaurant.entity.ts
@@ -0,0 +1,14 @@
+
+export class RestaurantEnity{
+
+    constructor(
+        public name:string,
+        public description:string,
+        public city: string,
+        public address:string,
+        public review:string[],
+        public contacts:string[],
+    ){}
+
+
+}
