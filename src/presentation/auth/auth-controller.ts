0bb50f03a5f7fb4040e38dfd2d48fa3a36f0c877
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-08-30 22:45:49 -0400

    Creating register user use case

--- src/presentation/auth/auth-controller.ts
+++ src/presentation/auth/auth-controller.ts
@@ -1,14 +1,63 @@
-import { Request } from "express";
+import { Request,Response } from "express";
+import { CustomError } from "../../domain/errors/ecustom.errors";
+import { RegisterUserDto } from "../../domain/dto/auth/register-user.dto";
+import { ResgisterUser } from "../../domain/use-cases/auth/register-user.use-case";
+import { LoginUserDto } from "../../domain/dto/auth/auth.dto";
+import { LoginUser } from "../../domain/use-cases/auth/login.use-case";
+import { UserModel } from "../../data/models/user";
+import { AuthRepository } from "../../domain/repositories/auth.repository";
 
 
 export class AuthController{
 
 
-    authUser=async(req:Request, res:Response)=>{
+
+        constructor(
+            private readonly authRepository:AuthRepository
+        ){}
+    
+        private handleError = (error:unknown, res:Response)=>{
+       
+            if(error instanceof CustomError){
+                return res.status(error.statusCode).json({error:error.message});
+            }
+            console.log(error);
+            return res.status(500).json({error:'Internal server error'});
+        }
+    
+        registerUser = async(req:Request, res:Response)=>{
+    
+            const [error,registerUserDto] = RegisterUserDto.create(req.body);
+    
+            if(error || !registerUserDto) return res.status(400).json({error})
+            
+    
+            new ResgisterUser(this.authRepository)
+            .execute(registerUserDto)
+            .then(data=>res.json(data)).catch(error=>this.handleError(error,res))
+    
+        }
+    
+        logginUser = async(req:Request, res:Response)=>{
+    
+            const [error,loginUserDto] = LoginUserDto.create(req.body);
+    
+            if(error || !loginUserDto) return res.status(400).json({error});
+    
+            new LoginUser(this.authRepository)
+            .execute(loginUserDto)
+            .then(data=>res.json(data)).catch(error=>this.handleError(error,res));
+    
+        }
+    
+        getUsers = async(req:Request, res:Response)=>{
+    
+            UserModel.find().then(users=>res.json({payload:req.body.user}))
+            .catch(()=>res.status(500).json({error:'Internal server error'}))
+    
+        }
+}
 
         
 
-    }
-
-
-}
+    
--- src/presentation/auth/auth-routes.ts
+++ src/presentation/auth/auth-routes.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { AuthRepositoryImpl } from "../../infraestructure/repositories/auth.repository";
+import { AuthController } from "./auth-controller";
+import { AuthDatasourceImpl } from "../../infraestructure/datasource/auth.datasource";
 
 
 export class AuthRoutes{
@@ -8,9 +11,18 @@ export class AuthRoutes{
         
 
         const router = Router();
-        router.post('/login',);
-        return router;
 
+        const dasource = new AuthDatasourceImpl();
+        const authRepository = new AuthRepositoryImpl(dasource)
+
+        const controller = new AuthController(authRepository);
+
+        router.post('/login',controller.logginUser);
+        router.post('/register',controller.registerUser);
+        
+       // router.get('/', AuthMiddleware.validateJwt,controller.getUsers)
+
+        return router;
         
     }
 
