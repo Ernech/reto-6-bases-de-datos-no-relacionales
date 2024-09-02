1c068e758f186ea1c87481b99b1ec2c677cd2335
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-09-02 18:15:41 -0400

    Changes in auth datasource

--- src/domain/datasource/auth.datasource.ts
+++ src/domain/datasource/auth.datasource.ts
@@ -9,7 +9,7 @@ export abstract class AuthDatasource{
     
     abstract login(loginUserDto:LoginUserDto):Promise<UserEntity>;
  
-    abstract register(registerUserDto:RegisterUserDto):Promise<[UserEntity,string]>;
+    abstract register(registerUserDto:RegisterUserDto):Promise<UserEntity>;
 
 
 }
