710fe752b308b167b9bb9e0f7193cf49e4e8a5f3
Ernesto Vilela <ernestovilela@MacBook-Pro-de-Ernesto.local>
2024-09-02 22:02:44 -0400

    Creating auth middleware

---
+++ src/presentation/middlewares/auth.middlewares.ts
@@ -0,0 +1,44 @@
+import { NextFunction, Request, Response } from "express";
+
+import { UserModel } from "../../data/models/user";
+import { JwtAdapter } from "../../config/jwt";
+
+export class AuthMiddleware{
+
+    static validateJwt = async(req:Request, res:Response, next:NextFunction)=>{
+
+        const authorization = req.header('Authorization');
+        if(!authorization) return res.status(401).json({error:'Token not provided'});
+        if(!authorization.startsWith('Bearer ')) return res.status(401).json({error:'Invalid token'});
+
+        const token = authorization.split(' ').at(1) || '';
+
+        try {
+            
+        
+
+            const payload = await  JwtAdapter.validateToken<{userId:string, roles:string[]}>(token); 
+
+            if(!payload) return res.status(401).json({error:'Invalid token!'})
+
+            
+            const user = await UserModel.findById(payload.userId);
+            
+
+            if(!user) return res.status(401).json({error:'Invalid token!!'})
+
+            req.body.user = user;
+
+            next();
+            
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({error:'Internal server error'});
+        }
+
+
+
+
+    }
+
+}
