549be99966145d04a3cd15cd515bf92a34793535
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2024-09-11 23:08:37 -0400

    Restaurant routes crud

--- src/presentation/restaurant/restaurant-routes.ts
+++ src/presentation/restaurant/restaurant-routes.ts
@@ -4,6 +4,7 @@ import { RestaurantRepositoryImpl } from "../../infraestructure/repositories/res
 import { ResturantController } from "./restaurant-controller";
 import { AuthMiddleware } from "../middlewares/auth.middlewares";
 import { CheckRoles } from "../middlewares/has-role.middleware";
+import { IsValidMongoId } from "../middlewares/mongoId.middleware";
 
 
 export class RestaurantRoutes{
@@ -14,10 +15,12 @@ export class RestaurantRoutes{
         const dataSource = new RestaurantDataSourceImpl();
         const repository = new RestaurantRepositoryImpl(dataSource);
         const restaurantController = new ResturantController(repository);
-
         router.post('/',[AuthMiddleware.validateJwt,CheckRoles.hasRole(['ADMIN_ROLE'])],restaurantController.createNewResturant);
-
-
+        router.put('/:id',[AuthMiddleware.validateJwt,CheckRoles.hasRole(['ADMIN_ROLE']),IsValidMongoId.checkId],restaurantController.editRestaurant);
+        router.delete('/:id',[AuthMiddleware.validateJwt,CheckRoles.hasRole(['ADMIN_ROLE']),IsValidMongoId.checkId],restaurantController.deleteRestaurant);
+        router.get('/:id',[IsValidMongoId.checkId],restaurantController.getRestaurantById);
+        router.get('/all',restaurantController.getRestaurants);
+        router.get('/search',restaurantController.searchRestaurant);
         return router;
 
         
