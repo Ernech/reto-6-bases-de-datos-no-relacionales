51476b59d1d5574fba36f4c639f7b6fec32ad232
Ernesto Vilela <ernestovilela@MacBook-Pro-de-Ernesto.local>
2024-09-17 21:52:13 -0400

    Update review use case

---
+++ src/domain/use-cases/review/update-review.use-case.ts
@@ -0,0 +1,21 @@
+import { ReviewDTO } from "../../dto/review/review.dto";
+import { ReviewEntity } from "../../entities/review.entity";
+import { ReviewRepository } from "../../repositories/review.reposotiry";
+
+
+interface UpdateReviewInterface{
+
+    execute(reviewDTO:ReviewDTO,reviewId:string,userId:string):Promise<ReviewEntity>;
+
+}
+
+
+export class UpdateReview implements UpdateReviewInterface{
+
+    constructor(private readonly reviewRepository:ReviewRepository){}
+        
+    execute(reviewDTO: ReviewDTO, reviewId: string, userId: string): Promise<ReviewEntity> {
+        return this.reviewRepository.updateReview(reviewDTO,reviewId,userId);
+    }
+
+}
